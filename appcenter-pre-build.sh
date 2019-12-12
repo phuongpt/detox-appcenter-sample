eb87c6d48dec8abae84c8bf075c1f41343f05198
Phuong Phan <phuongphantai@gmail.com>
2019-12-12 17:49:38 +0700

    Try improve pre-build

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,6 +2,12 @@
 
 echo "PRE BUILD STARTED"
 
+echo "iOS: Building the project for Detox tests..."
+yarn detox build --configuration ios.sim.release
+
+echo "iOS: Cleaning cache for Detox tests..."
+detox clean-framework-cache && detox build-framework-cache
+
 echo "iOS: Executing Detox tests..."
 yarn detox test --configuration ios.sim.release --cleanup
 
