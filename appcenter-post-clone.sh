b3cadae2178f3bd9debe6f901d6c61689b682d2f
Phuong Phan <phuongphantai@gmail.com>
2019-12-12 19:53:57 +0700

    try test

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -6,14 +6,14 @@ echo "iOS: Installing applesimutils..."
 brew tap wix/brew
 brew install wix/brew/applesimutils
 
-echo "Installing NVM..."
-brew install nvm
-source $(brew --prefix nvm)/nvm.sh
+# echo "Installing NVM..."
+# brew install nvm
+# source $(brew --prefix nvm)/nvm.sh
 
-echo "Installing v10.15.1..."
-nvm install v10.15.1
-nvm use --delete-prefix v10.15.1
-nvm alias default v10.15.1
+# echo "Installing v10.15.1..."
+# nvm install v10.15.1
+# nvm use --delete-prefix v10.15.1
+# nvm alias default v10.15.1
 
 echo "Identifying selected node version..."
 node --version
@@ -21,18 +21,18 @@ node --version
 echo "Installing detox cli..."
 npm install -g detox-cli
 
-echo "Installing dependencies for detox tests..."
-yarn
+# echo "Installing dependencies for detox tests..."
+# yarn
 
-echo "iOS: Update pod"
-cd ios  
-pod install
-cd ..
+# echo "iOS: Update pod"
+# cd ios  
+# pod install
+# cd ..
 
-echo "iOS: Building the project for Detox tests..."
-yarn detox build --configuration ios.sim.release
+# echo "iOS: Building the project for Detox tests..."
+# yarn detox build --configuration ios.sim.release
 
-echo "iOS: Cleaning cache for Detox tests..."
-detox clean-framework-cache && detox build-framework-cache
+# echo "iOS: Cleaning cache for Detox tests..."
+# detox clean-framework-cache && detox build-framework-cache
 
 echo "POST CLONE COMPLETED"
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,6 +2,12 @@
 
 echo "PRE BUILD STARTED"
 
+echo "iOS: Building the project for Detox tests..."
+npx detox build --configuration ios.sim.release
+
+echo "iOS: Cleaning cache for Detox tests..."
+npx clean-framework-cache && detox build-framework-cache
+
 echo "iOS: Executing Detox tests..."
 npx detox test --configuration ios.sim.release --cleanup
 
