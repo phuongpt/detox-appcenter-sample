26a5e686876ed2ec22a611618666d933a6885157
Phuong Phan <phuongphantai@gmail.com>
2019-12-12 14:10:44 +0700

    Move testing to post-build

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -5,4 +5,32 @@ echo "POST BUILD STARTED"
 git diff > xxx
 cat xxx
 
+echo "iOS: Installing applesimutils..."
+brew tap wix/brew
+brew install wix/brew/applesimutils
+
+echo "Installing NVM..."
+brew install nvm
+source $(brew --prefix nvm)/nvm.sh
+
+echo "Installing v10.15.1..."
+nvm install v10.15.1
+nvm use --delete-prefix v10.15.1
+nvm alias default v10.15.1
+
+echo "Identifying selected node version..."
+node --version
+
+echo "Installing detox cli..."
+npm install -g detox-cli
+
+echo "Installing dependencies for detox tests..."
+yarn
+
+echo "iOS: Building the project for Detox tests..."
+yarn detox build --configuration ios.sim.release
+
+echo "iOS: Executing Detox tests..."
+yarn detox test --configuration ios.sim.release --cleanup
+
 echo "POST BUILD COMPLETED"
