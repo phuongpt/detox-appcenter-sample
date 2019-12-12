7003fed3e58c259b4b8eb1c0f3751677e77825aa
Phuong Phan <phuongphantai@gmail.com>
2019-12-12 20:13:28 +0700

    try pre-build

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,37 +2,14 @@
 
 echo "POST CLONE STARTED"
 
-echo "iOS: Installing applesimutils..."
-brew tap wix/brew
-brew install wix/brew/applesimutils
+# echo "iOS: Installing applesimutils..."
+# brew tap wix/brew
+# brew install wix/brew/applesimutils
 
-# echo "Installing NVM..."
-# brew install nvm
-# source $(brew --prefix nvm)/nvm.sh
+# echo "Identifying selected node version..."
+# node --version
 
-# echo "Installing v10.15.1..."
-# nvm install v10.15.1
-# nvm use --delete-prefix v10.15.1
-# nvm alias default v10.15.1
-
-echo "Identifying selected node version..."
-node --version
-
-echo "Installing detox cli..."
-npm install -g detox-cli
-
-# echo "Installing dependencies for detox tests..."
-# yarn
-
-# echo "iOS: Update pod"
-# cd ios  
-# pod install
-# cd ..
-
-# echo "iOS: Building the project for Detox tests..."
-# yarn detox build --configuration ios.sim.release
-
-# echo "iOS: Cleaning cache for Detox tests..."
-# detox clean-framework-cache && detox build-framework-cache
+# echo "Installing detox cli..."
+# npm install -g detox-cli
 
 echo "POST CLONE COMPLETED"
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,6 +2,16 @@
 
 echo "PRE BUILD STARTED"
 
+echo "iOS: Installing applesimutils..."
+brew tap wix/brew
+brew install wix/brew/applesimutils
+
+echo "Identifying selected node version..."
+node --version
+
+echo "Installing detox cli..."
+npm install -g detox-cli
+
 echo "iOS: Update pod"
 cd ios  
 pod install
